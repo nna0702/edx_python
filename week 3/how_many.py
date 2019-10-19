f7d5dc236e7fe7b5f2953216d8d0958b7f8d6ae4
Nguyet Anh Nguyen <nna0702@gmail.com>
2019-10-19 15:53:52 -0400

    add description for how_many

--- week 3/how_many.py
+++ week 3/how_many.py
@@ -1,3 +1,8 @@
+# We want to write some simple procedures that work on dictionaries to return information.
+# 
+# First, write a procedure, called how_many, which returns the sum of the number of values associated with a dictionary.
+
+
 def how_many(aDict):
     '''
     aDict: A dictionary, where all the values are lists.
